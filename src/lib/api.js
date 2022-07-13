37b34d7c078e75eb96c91efac249f2db8f19a691
Moisés Villagrán <103071492+moevillagran@users.noreply.github.com>
2022-07-12 18:27:57 -0600

    Add files via upload

---
+++ src/lib/api.js
@@ -0,0 +1,25 @@
+import axios from "axios";
+
+export async function getTodos() {
+try {
+    const {
+    data: { todos },
+    } = await axios.get("http://localhost:3000/api/v1/to-dos");
+    return todos;
+} catch (_error) {
+    return [];
+}
+}
+
+export async function updateTodoData(todoId, todoData) {
+try {
+    console.log("HERE", todoData);
+    await axios.patch(`http://localhost:3000/api/v1/to-dos/${todoId}`, {
+    ...todoData,
+    });
+} catch ({ response }) {
+    console.log(response);
+    throw new Error(response.data.message);
+    }
+}
+
